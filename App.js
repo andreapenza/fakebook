497b56e59730f61ad7f31466aecc47edac50f203
Andrea <andrea.penza94@gmail.com>
2020-05-22 17:28:25 +0200

    start project

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
